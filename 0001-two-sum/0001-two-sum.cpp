611233fa739eb918861c34d9d0451d2fc839cd15
Parth Bhanti <167682633+parthbhanti22@users.noreply.github.com>
2025-08-01 01:03:06 +0530

    Time: 0 ms (100%), Space: 14.9 MB (36.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,14 +4,11 @@ public:
         unordered_map<int,int> umap;
         int n = nums.size();
         for(int i=0;i<n;i++){
-            umap[nums[i]] = i;
-        }
-
-        for(int i=0;i<n;i++){
             int complement = target - nums[i];
             if(umap.count(complement) && umap[complement]!=i){
                 return {i,umap[complement]};
             }
+            umap[nums[i]] = i;
             
         }
 
